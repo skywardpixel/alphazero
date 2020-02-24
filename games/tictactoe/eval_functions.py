6d942ce5f3c4bfd9189e6876a749435ed90cbada
Kyle <kyleyan@cs.washington.edu>
2020-02-24 10:31:32 -0800

    Add better eval func for tic-tac-toe

--- games/tictactoe/eval_functions.py
+++ games/tictactoe/eval_functions.py
@@ -1,4 +1,4 @@
-from games.tictactoe import TicTacToeGameState, TicTacToePlayer
+from games.tictactoe import TicTacToeGameState, TicTacToePlayer, TicTacToeBoard
 
 
 def simple_eval_func(state: TicTacToeGameState, player: TicTacToePlayer) -> float:
@@ -10,3 +10,74 @@ def simple_eval_func(state: TicTacToeGameState, player: TicTacToePlayer) -> floa
         return -99999.
     else:
         return 0.
+
+
+def better_eval_func(state: TicTacToeGameState, player: TicTacToePlayer) -> float:
+    if state.is_tie():
+        return 0.
+    if state.current_player == player:
+        if state.is_win():
+            return 99999.
+        elif state.is_lose():
+            return -99999.
+    else:
+        if state.is_win():
+            return -99999.
+        elif state.is_lose():
+            return 99999.
+
+    def count_good_rows(board: TicTacToeBoard, count_player: TicTacToePlayer):
+        num_good_rows = 0
+        for r in range(board.size):
+            empty, adversary = 0, 0
+            for c in range(board.size):
+                if board.get(r, c) == count_player.opponent:
+                    adversary += 1
+                elif board.get(r, c) is None:
+                    empty += 1
+            if empty == 1 and adversary == 0:
+                num_good_rows += 1
+        return num_good_rows
+
+    def count_good_columns(board: TicTacToeBoard, count_player: TicTacToePlayer):
+        num_good_columns = 0
+        for c in range(board.size):
+            empty, adversary = 0, 0
+            for r in range(board.size):
+                if board.get(r, c) == count_player.opponent:
+                    adversary += 1
+                elif board.get(r, c) is None:
+                    empty += 1
+            if empty == 1 and adversary == 0:
+                num_good_columns += 1
+        return num_good_columns
+
+    def count_good_diagonals(board: TicTacToeBoard, count_player: TicTacToePlayer):
+        num_good_diagonals = 0
+        # major
+        empty, adversary = 0, 0
+        for i in range(board.size):
+            if board.get(i, i) == count_player.opponent:
+                adversary += 1
+            elif board.get(i, i) is None:
+                empty += 1
+        if empty == 1 and adversary == 0:
+            num_good_diagonals += 1
+        # minor
+        empty, adversary = 0, 0
+        for i in range(board.size):
+            if board.get(board.size - 1 - i, i) == count_player.opponent:
+                adversary += 1
+            elif board.get(i, i) is None:
+                empty += 1
+        if empty == 1 and adversary == 0:
+            num_good_diagonals += 1
+        return num_good_diagonals
+
+    board = state.board
+    return 100. * (count_good_rows(board, player)
+                   + count_good_columns(board, player)
+                   + count_good_diagonals(board, player)
+                   - count_good_rows(board, player.opponent)
+                   - count_good_columns(board, player.opponent)
+                   - count_good_diagonals(board, player.opponent))
--- play_tictactoe.py
+++ play_tictactoe.py
@@ -1,6 +1,12 @@
+import os
+
 from agents.minimax.alphabeta import AlphaBetaAgent
-from games.tictactoe.eval_functions import simple_eval_func
+from games.tictactoe.eval_functions import simple_eval_func, better_eval_func
 from games.tictactoe.tictactoe import TicTacToeGame, TicTacToeMove, TicTacToePlayer
+
+
+def clear():
+    os.system('cls') if os.name == 'nt' else os.system('clear')
 
 
 def read_move(player: TicTacToePlayer) -> TicTacToeMove:
@@ -11,10 +17,12 @@ def read_move(player: TicTacToePlayer) -> TicTacToeMove:
 
 game = TicTacToeGame()
 # TODO: fix typing for eval_fn
-agent = AlphaBetaAgent(TicTacToePlayer.O, depth=6, eval_fn=simple_eval_func)
+agent = AlphaBetaAgent(TicTacToePlayer.O, depth=6, eval_fn=better_eval_func)
 
 while not game.is_over:
+    clear()
     game.show_board()
+    # print(f"current state score by eval func: {agent.eval_fn(game.state, agent.player)}")
     if game.current_player == TicTacToePlayer.X:
         move = read_move(game.current_player)
         while not game.state.board.is_legal_move(move):
@@ -27,6 +35,7 @@ while not game.is_over:
 
 print("--- GAME OVER ---")
 game.show_board()
+print(f"current state score by eval func: {agent.eval_fn(game.state, agent.player)}")
 if game.winner:
     print(f"{game.winner.name} wins!")
 else:
